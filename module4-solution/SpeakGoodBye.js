5f982cf1b7027beac8284d3699c6bb4da30bacb5
Rman <rman@Mac-Rman.local>
2017-01-29 22:24:50 +0300

    2

--- module4-solution/SpeakGoodBye.js
+++ module4-solution/SpeakGoodBye.js
@@ -5,5 +5,5 @@
           console.log(speakWord + " " + name);  
     }
     window.byeSpeaker = byeSpeaker;
- })(window);
+})(window);
 
--- module4-solution/script.js
+++ module4-solution/script.js
@@ -1,5 +1,5 @@
 
-(function (window) {
+(function () {
     var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 
     for (var i = 0; i < names.length; i++) {
@@ -11,6 +11,6 @@
             helloSpeaker.speak(names[i]);
         }
     }
-})(window); 
+})(); 
 
 
